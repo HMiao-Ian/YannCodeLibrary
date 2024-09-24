cca94c5d25892b632d43c649fc7f58ffcafff89c
HMiao-Ian <hongmiao.ian@gmail.com>
2024-09-24 22:03:16 +0800

    2024.09.24 22:03

--- TheCppPrimerPlus/test.cpp
+++ TheCppPrimerPlus/test.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 {
 	cout << "Hello, world!";
+	cout << "\n";
+	cout << "This is a C++ program.";
+    return 0; // exit status 0 indicates successful execution
 }
 
 
